e49033c3626b672a6a59af787b17d944238c2478
Wirods <wirods.pp@gmail.com>
2020-10-26 00:39:05 +0700

    Update show ingredient detail

--- frontend/src/component/Menu/IngredientList.jsx
+++ frontend/src/component/Menu/IngredientList.jsx
@@ -4,7 +4,7 @@ export default function IngredientList(props) {
   return (
     <div className="container">
       <div className="col-2">
-        <h5>Ingredient</h5>
+        <h5>ส่วนผสมหลัก</h5>
       </div>
       <div className="d-flex justify-content-around">
         {props.menu?.ingredient?.map((ingredient) => {
@@ -21,6 +21,27 @@ export default function IngredientList(props) {
             </div>
           )
         })}
+      </div>
+      <div className="col-2">
+        <h5>ส่วนผสมเพิ่มเติม</h5>
+      </div>
+      <div className="col-8">
+      <div className="d-flex">
+        {props.ingr?.map((ingredient) => {
+          return (
+            <div className="p-2">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="exampleRadios"
+                id="exampleRadios2"
+                value="option2"
+              />
+              {ingredient.ingredient_name}
+            </div>
+          )
+        })}
+      </div>
       </div>
     </div>
   )
